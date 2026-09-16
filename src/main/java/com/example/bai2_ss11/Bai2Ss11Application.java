81028fb8046ac7e8eb75287c312c69a141773a5d
Hoang Cuong Cat <cathoangfb1@gmail.com>
2026-09-16 20:19:25 +0700

    Initial commit

---
+++ src/main/java/com/example/bai2_ss11/Bai2Ss11Application.java
@@ -0,0 +1,13 @@
+package com.example.bai2_ss11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bai2Ss11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bai2Ss11Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bai2_ss11/Bai2Ss11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bai2_ss11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bai2Ss11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
